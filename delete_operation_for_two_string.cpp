8e5dede1813a47032f7e0c9e3f5930fa87f87b79
Vijay Chathe <76934330+vijay1213@users.noreply.github.com>
2022-07-28 11:55:08 +0530

    Create delete_operation_for_two_string.cpp

---
+++ delete_operation_for_two_string.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int lcs(string s1, string s2) {
+    
+    int n=s1.size();
+    int m=s2.size();
+
+    vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
+    for(int i=0;i<=n;i++){
+        dp[i][0] = 0;
+    }
+    for(int i=0;i<=m;i++){
+        dp[0][i] = 0;
+    }
+    
+    for(int ind1=1;ind1<=n;ind1++){
+        for(int ind2=1;ind2<=m;ind2++){
+            if(s1[ind1-1]==s2[ind2-1])
+                dp[ind1][ind2] = 1 + dp[ind1-1][ind2-1];
+            else
+                dp[ind1][ind2] = 0 + max(dp[ind1-1][ind2],dp[ind1][ind2-1]);
+        }
+    }
+    
+    return dp[n][m];
+}
+    int minDistance(string word1, string word2) {
+        
+      int n = word1.size();
+        int m = word2.size();
+        cout << lcs(word1, word2);
+        return n + m - 2*lcs(word1, word2);
+        
+    }
+};
